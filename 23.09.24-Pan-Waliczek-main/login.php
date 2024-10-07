8be6ed1f2bb7c41fe6cc73c316e5d57ac5c7512e
Sojerowak <144764824+Sojerowak@users.noreply.github.com>
2024-10-07 10:38:17 +0200

    Ap

---
+++ 23.09.24-Pan-Waliczek-main/login.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+include('includes/db.php');
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $query = $conn->prepare("SELECT * FROM users WHERE username = ?");
+    $query->bind_param('s', $username);
+    $query->execute();
+    $result = $query->get_result();
+    if ($result->num_rows > 0) {
+        $user = $result->fetch_assoc();
+        if (password_verify($password, $user['password'])) {
+            $_SESSION['user_id'] = $user['id'];
+            header('Location: index.php');
+            exit;
+        } else {
+            $error = 'nieprawne hasło' . $user['password'] . $username;
+        }
+    }else{
+        $error = 'nieznany użytkownik';
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="login-container">
+        <form action="login.php" method="post">
+            <h2>logowanie</h2>
+            <?php if (isset($error)) { echo "<p class='error'>$error</p>"; }?>
+            <label for="username">Nazwa użytkownika:</label>
+            <input type="text" id="username" name="username" required>
+            <label for="password">Hasło:</label>
+            <input type="password" id="password" name="password" required>
+            <button type="submit">Zaloguj się</button>
+        </form>
+    </div>
+</body>
+</html>
